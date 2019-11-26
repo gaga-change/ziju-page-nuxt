7b5fde5b5c7003f80290f98e183633634bd55a35
严俊东 <1172482914@qq.com>
2019-11-26 17:19:14 +0800

    主内容展示提取为公共组件

--- pages/index.vue
+++ pages/index.vue
@@ -1,46 +1,16 @@
 <template>
   <div class="PageIndex">
-    <div>
-      <div ref="grid">
-        <div
-          class="grid-item"
-          v-for="(item) in posts"
-          :key="item._id"
-          style="width:236px"
-        >
-        <router-link :to="{name: 'posts-id', params: {id: item._id}}">
-          <el-card :body-style="{ padding: '0px' }">
-            <img
-              :src="item.imgUrl"
-              class="image"
-            >
-            <div style="padding: 14px;">
-              <span>{{item.title}}</span>
-              <div class="bottom clearfix">
-                <time class="time">{{ item.createdAt | date }}</time>
-                <!-- <el-button
-                  type="text"
-                  class="button"
-                >操作按钮</el-button> -->
-              </div>
-            </div>
-          </el-card>
-          </router-link>
-        </div>
-      </div>
-    </div>
+    <PostList :posts="posts"></PostList>
   </div>
 </template>
 
 <script>
-let Masonry = null;
-if (process.client) {
-  Masonry = require("masonry-layout");
-}
+import PostList from "@/components/PostList";
 
 export default {
+  components: { PostList },
   async fetch({ store, params }) {
-    await Promise.all([store.dispatch("initWeb")])
+    await Promise.all([store.dispatch("initWeb")]);
   },
   async asyncData({ $axios, req }) {
     const { list } = await $axios.$get("/api/posts?pageSize=999");
@@ -51,54 +21,8 @@ export default {
   },
   head() {
     return {
-      title: '紫菊 - 首页'
+      title: "紫菊 - 首页"
     };
-  },
-  mounted() {
-    var grid = this.$refs["grid"];
-    var msnry = new Masonry(grid, {
-      gutter: 18,
-      itemSelector: ".grid-item",
-      columnWidth: 236
-    });
   }
 };
 </script>
-
-<style lang="less">
-.PageIndex {
-  .el-row {
-    margin-bottom: 20px;
-    &:last-child {
-      margin-bottom: 0;
-    }
-  }
-
-  .grid-item {
-    margin-bottom: 10px;
-    a {
-      text-decoration: none;
-    }
-  }
-
-  .time {
-    font-size: 13px;
-    color: #999;
-  }
-
-  .bottom {
-    margin-top: 13px;
-    line-height: 12px;
-  }
-
-  .button {
-    padding: 0;
-    float: right;
-  }
-
-  .image {
-    width: 100%;
-    display: block;
-  }
-}
-</style>
